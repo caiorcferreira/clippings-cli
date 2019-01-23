175c5ae8b362f1be1231461e7d04126f7d633a33
Caio Ferreira <caio.cavalcante@b2wdigital.com>
2019-01-23 21:47:09 -0200

    [refact] add descriptions and examples to cli commands

--- cmd/database.go
+++ cmd/database.go
@@ -9,8 +9,8 @@ var createDatabaseOutputFile string
 var queryDatabaseOutputFile string
 
 func init() {
-	databaseCreateCmd.Flags().StringVarP(&createDatabaseOutputFile, "output-file", "o", "", "file to write the database")
-	databaseQueryCmd.Flags().StringVarP(&queryDatabaseOutputFile, "output-file", "o", "", "file to write the database")
+	databaseCreateCmd.Flags().StringVarP(&createDatabaseOutputFile, "output-file", "o", "", "file to write the created database")
+	databaseQueryCmd.Flags().StringVarP(&queryDatabaseOutputFile, "output-file", "o", "", "file to write the query results")
 
 	databaseCmd.AddCommand(databaseCreateCmd)
 	databaseCmd.AddCommand(databaseUpdateCmd)
@@ -20,11 +20,13 @@ func init() {
 
 var databaseCmd = &cobra.Command{
 	Use: "database",
-	Short: "",
+	Short: "Manage the JSON Clippings database",
 }
 
 var databaseCreateCmd = &cobra.Command{
-	Use: "create",
+	Use:                    "create",
+	Short:                  "Create a new JSON database from a raw clippings file",
+	Example:                "clippings database create [-o DATABASE_PATH] <CLIPPINGS_PATH>",
 	Run: func(cmd *cobra.Command, args []string) {
 		app := clippings.NewApp()
 
@@ -34,6 +36,11 @@ var databaseCreateCmd = &cobra.Command{
 
 var databaseUpdateCmd = &cobra.Command{
 	Use: "update",
+	Short: "Updates an existing database with raw clippings from a file",
+	Long: `Updates a JSON database with raw clippings from a file.
+			The tool uses a simple hashing algorithm in order to avoid inserting clippings that already exists in the database.
+			But notice that sometimes Kindle introduces duplicated clippings that cannot be avoided be this mechanism.`,
+	Example: "clippings database update <DATABASE_PATH> <CLIPPINGS_PATH>",
 	Run: func(cmd *cobra.Command, args []string) {
 		app := clippings.NewApp()
 
@@ -43,6 +50,10 @@ var databaseUpdateCmd = &cobra.Command{
 
 var databaseQueryCmd = &cobra.Command{
 	Use: "query",
+	Short: "Executes a query against a JSON database",
+	Long: `Executes a query using path syntax for search against a JSON database, optionally writing the query results to a given file.
+			To see more about the path syntax refer to https://github.com/tidwall/gjson#path-syntax`,
+	Example: "clippings database query [-o OUTPUT_FILE] <QUERY> <DATABASE_PATH>",
 	Run: func(cmd *cobra.Command, args []string) {
 		app := clippings.NewApp()
 
